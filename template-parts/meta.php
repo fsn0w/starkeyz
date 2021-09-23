015ba47fecdd1d577564ca285d4adb9d24043aa7
Kyryll Prytula <k.prytula@gmail.com>
2021-09-23 20:53:50 +0300

    bbb

--- template-parts/meta.php
+++ template-parts/meta.php
@@ -16,7 +16,8 @@
 
     <!-- Get some bootstrap default styles -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-  </head>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>  
+</head>
   <body>
     <!-- Construct a Bootstrap layout -->
     <div class="container">
