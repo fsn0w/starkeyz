7375a30f23e453a0cbd151b86e36afe1a947c21b
Kyryll Prytula <k.prytula@gmail.com>
2021-10-30 12:19:39 +0300

    css v-3.11

--- functions.php
+++ functions.php
@@ -41,7 +41,7 @@ add_action( 'after_setup_theme', 'tailpress_setup' );
 function tailpress_enqueue_scripts() {
 	$theme = wp_get_theme();
 
-	wp_enqueue_style( 'tailpress', tailpress_asset( 'css/app.css' ), array(),'3.09' );
+	wp_enqueue_style( 'tailpress', tailpress_asset( 'css/app.css' ), array(),'3.11' );
 	wp_enqueue_script( 'tailpress', tailpress_asset( 'js/app.js' ), array(), $theme->get( 'Version' ) );
 }
 
