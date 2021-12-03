b1d17614bd5c28bf1b2c3d990b593802a442854a
Kyryll Prytula <k.prytula@gmail.com>
2021-12-03 23:12:33 +0200

    faq

--- template-parts/faq.php
+++ template-parts/faq.php
@@ -8,9 +8,103 @@ get_header();
 
 ?>
 
+<div id="content" class="relative site-content flex flex-grow flex-col bg-no-repeat bg-cover bg-top lg:min-h-screen overflow-y-hidden" style="background-image:url(<?=get_stylesheet_directory_uri()?>/assets/img/faq-bg.jpg);">
+    <div class="w-full relative">
+            <div class="container mx-auto pt-32 flex justify-center items-center lg:pt-40">
+                <h1 class="font-zrnic text-4xl px-5 lg:text-6xl">FAQ's</h1>
+                <img src="<?=get_stylesheet_directory_uri()?>/assets/img/faq-main.png" alt="FAQs" class="w-40 lg:w-72" />
+            </div>
 
-abc
+            <div class="container mx-auto pb-20">
 
+                <div class="accordion mt-10">
+                    <div class="accordion-item border-t">
+                    <button aria-expanded="false"><span class="accordion-title">What is the Starkeyz?</span><span class="icon" aria-hidden="true"></span></button>
+                        <div class="accordion-content">
+                            <p>Starkeyz is a set of 10,000 unique digital Starkey collectibles living on the Ethereum blockchain (NFTs).<br><br>
+Each Starkey has a different combination of traits (different backgrounds, colours, key heads, blades and bitting cuts) - some much rarer than others.<br><br>
+The Starkeys collection is divided into 10 rarity-level categories, and each category has its own minting price (ranging from free for the most common ones and up to 1ETH for the rarest category).</p>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                    <button aria-expanded="false"><span class="accordion-title">What can I do with my Starkeyz?</span><span class="icon" aria-hidden="true"></span></button>
+                        <div class="accordion-content">
+                            <p>Your Starkeyz NFT is like any other NFT and you are free to use it however you choose to under a non-exclusive license.
+<br><br>
+In the later stages of the project, your unique Starkyz NFT will give you access to a dedicated safe and it will unlock its content.</p>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                    <button aria-expanded="false"><span class="accordion-title">Where can i find my purchased Starkeyz?</span><span class="icon" aria-hidden="true"></span></button>
+                        <div class="accordion-content">
+                            <p>Like any other NFT, your Starkeyz will appear in the connected wallet/address you used for the purchase.</p>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                    <button aria-expanded="false"><span class="accordion-title">Who stands behind the Starkeyz project?</span><span class="icon" aria-hidden="true"></span></button>
+                        <div class="accordion-content">
+                            <p>We are a group of visionaries and dreamers that are on a mission to take NFTs to a whole different dimension. From artists to gamers, and from coders to blockchain experts - we have everything it takes to challenge the world as we know it.</p>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                    <button aria-expanded="false"><span class="accordion-title">What's inside the safe?</span><span class="icon" aria-hidden="true"></span></button>
+                        <div class="accordion-content">
+                            <p>The safe's content will be revealed at a later stage, and only once more than 90% of the keys are minted.</p>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                    <button aria-expanded="false"><span class="accordion-title">How many Starkeyz can I mint at the same time?</span><span class="icon" aria-hidden="true"></span></button>
+                        <div class="accordion-content">
+                            <p>There's a mint limit of 5 Starkeyz.</p>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                    <button aria-expanded="false"><span class="accordion-title">Where can I interact with & meet other community members?</span><span class="icon" aria-hidden="true"></span></button>
+                        <div class="accordion-content">
+                            <p class="footer-social">
+
+            <span class="inline-block text-center"><a href="https://twitter.com/StarKeyz_" target="_blank" class="inline-flex justify-center items-center w-16 h-16 mx-4 mb-3 rounded-full"><svg width="24" height="19" viewBox="0 0 24 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M23.9178 2.44844C23.0905 2.80482 22.145 3.04241 21.1995 3.1612C22.145 2.56724 22.9723 1.61691 23.3269 0.547785C22.3814 1.14174 21.4359 1.49812 20.2541 1.7357C19.4268 0.785368 18.1267 0.191406 16.8267 0.191406C14.2266 0.191406 12.0992 2.32966 12.0992 4.94307C12.0992 5.29945 12.0992 5.65582 12.2174 6.01219C8.3173 5.77461 4.77172 3.87394 2.40801 1.02294C2.05345 1.73569 1.81708 2.56724 1.81708 3.39878C1.81708 5.06186 2.64439 6.48736 3.94443 7.31891C3.11713 7.31891 2.40801 7.08132 1.81708 6.72495C1.81708 8.98199 3.47168 11.0014 5.59902 11.3578C5.24446 11.4766 4.77173 11.4766 4.29899 11.4766C3.94443 11.4766 3.70806 11.4766 3.3535 11.3578C3.94443 13.2585 5.71722 14.684 7.84456 14.684C6.18996 15.9907 4.1808 16.7034 1.93527 16.7034C1.58072 16.7034 1.22616 16.7035 0.753418 16.5847C2.88076 17.8914 5.36266 18.7229 8.08093 18.7229C16.8267 18.7229 21.5541 11.4766 21.5541 5.18065V4.58669C22.4996 4.23032 23.3269 3.39877 23.9178 2.44844Z" fill="white"/>
+			</svg></a> <br> twitter </span>
+
+			<span class="inline-block text-center"><a href="https://t.me/joinchat/v8BQPybRwcM4ZDM9" target="_blank" class="inline-flex justify-center items-center w-16 h-16 mx-4 mb-3 rounded-full"><svg width="24" height="20" viewBox="0 0 24 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M2.18184 8.84839C8.4016 6.13854 12.5491 4.35203 14.6243 3.48889C20.5494 1.02442 21.7806 0.596318 22.5831 0.582182C22.7596 0.579073 23.1542 0.622814 23.4098 0.830237C23.6257 1.00538 23.6851 1.24198 23.7135 1.40803C23.7419 1.57409 23.7773 1.95237 23.7492 2.24794C23.4281 5.62161 22.0388 13.8086 21.3319 17.5872C21.0329 19.186 20.444 19.7221 19.8739 19.7746C18.6349 19.8886 17.694 18.9557 16.494 18.1691C14.6162 16.9382 13.5553 16.1719 11.7326 14.9708C9.62609 13.5826 10.9916 12.8197 12.1921 11.5728C12.5063 11.2465 17.9654 6.28105 18.071 5.83061C18.0842 5.77427 18.0965 5.56428 17.9717 5.4534C17.847 5.34251 17.6629 5.38043 17.53 5.41059C17.3417 5.45333 14.3418 7.43611 8.53051 11.3589C7.67902 11.9436 6.90777 12.2285 6.21675 12.2136C5.45496 12.1971 3.98958 11.7828 2.90022 11.4287C1.56407 10.9944 0.502131 10.7648 0.594603 10.0271C0.642767 9.64295 1.17185 9.25003 2.18184 8.84839Z" fill="white"/>
+            </svg>
+            </a> <br> telegram </span>
+
+			<span class="inline-block text-center"><a href="https://discord.gg/BqSNp2ZAKP" target="_blank" class="inline-flex justify-center items-center w-16 h-16 mx-4 mb-3 rounded-full"><svg width="28" height="21" viewBox="0 0 28 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M7.25317 20.089C7.25317 20.089 8.10067 19.0415 8.79123 18.1685C5.87205 17.2797 4.77344 15.4226 4.77344 15.4226C4.77344 15.4226 5.00885 15.5813 5.41691 15.8194C5.43261 15.8353 5.464 15.867 5.51108 15.8829C5.57386 15.9305 5.65233 15.9464 5.71511 15.994C6.2958 16.3115 6.86081 16.5813 7.39442 16.7876C8.33609 17.1527 9.4661 17.5336 10.7687 17.7876C12.4951 18.105 14.504 18.232 16.717 17.8034C17.7999 17.613 18.8985 17.2955 20.0442 16.8035C20.8446 16.5019 21.7392 16.0575 22.6809 15.4385C22.6809 15.4385 21.5352 17.3432 18.5218 18.2002C19.2124 19.0891 20.0442 20.0732 20.0442 20.0732C25.1135 19.9144 27.0753 16.5495 27.0753 16.5495C27.0753 9.07377 23.7638 3.01062 23.7638 3.01062C20.4523 0.518692 17.3134 0.58218 17.3134 0.58218L16.9995 0.94724C20.9074 2.15352 22.7123 3.89946 22.7123 3.89946C20.3267 2.58207 17.9882 1.93131 15.8067 1.67736C14.1431 1.48689 12.5579 1.53451 11.1611 1.72497C11.0199 1.72497 10.91 1.74085 10.7687 1.77259C9.96832 1.83608 8.00651 2.13765 5.55816 3.23283C4.71066 3.62963 4.20843 3.89946 4.20843 3.89946C4.20843 3.89946 6.10747 2.07416 10.2351 0.867879L10.0154 0.58218C10.0154 0.58218 6.8765 0.518692 3.56496 3.02649C3.56496 3.02649 0.253418 9.08964 0.253418 16.5654C0.253418 16.5495 2.18384 19.9303 7.25317 20.089ZM17.7528 9.24836C19.0554 9.24836 20.0913 10.407 20.0913 11.8196C20.0913 13.2323 19.0554 14.3909 17.7528 14.3909C16.4658 14.3909 15.4143 13.2323 15.4143 11.8196C15.3986 10.407 16.4345 9.24836 17.7528 9.24836ZM9.37193 9.24836C10.6746 9.24836 11.7418 10.407 11.7104 11.8196C11.7104 13.2323 10.6746 14.3909 9.37193 14.3909C8.08498 14.3909 7.03345 13.2323 7.03345 11.8196C7.03345 10.407 8.05359 9.24836 9.37193 9.24836Z" fill="white"/>
+            </svg>
+            </a> <br> discord </span>
+
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+    </div>
+</div>
+
+<script>
+    const items = document.querySelectorAll(".accordion button");
+    function toggleAccordion() {
+        const itemToggle = this.getAttribute('aria-expanded');
+        if (itemToggle == 'false') {
+            this.setAttribute('aria-expanded', 'true');
+        } else {
+            this.setAttribute('aria-expanded', 'false');
+        }
+    }
+    items.forEach(item => item.addEventListener('click', toggleAccordion));
+</script>
 
 <?php
 
